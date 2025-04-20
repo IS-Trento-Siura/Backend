fc9d358f3c948a27f80dd03a97dba0707e0fd46d
Giieck <g.petracin@gmail.com>
2025-04-20 12:23:57 +0200

    User

---
+++ models/OrgModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+const collection = 'Orgs'; 
+
+const OrgSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: Number, required: true, unique: true },
+    indirizzo: { type: String, required: true }, 
+    segnalazioni: [{ type: mongoose.Schema.Types.ObjectId, ref: "Report", default: [] }], 
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model(collection, OrgSchema);
---
+++ models/UserModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+const collection = 'Users';
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: Number, required: false },
+    posizione: { type: Boolean, required: false, default: false }, 
+    segnalazioni: [{ type: mongoose.Schema.Types.ObjectId, ref: "Report", default: [] }], 
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model(collection, UserSchema);
