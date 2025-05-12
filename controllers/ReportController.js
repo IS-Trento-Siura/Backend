b445f89c6a0a7ba20c3c1e5fd9d54b8957cd6520
Giieck <g.petracin@gmail.com>
2025-05-12 22:27:31 +0200

    update_and_get_reports

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -1,14 +1,12 @@
 import User from '../models/UserModel.js';
 import Report from '../models/ReportModel.js';
+import e from 'express';
+
 
 export const createReport = async (req, res) => {
   try {
-    const { userId, reportData } = req.body;
-
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
+    const { reportData } = req.body;
+    const userId = req.user.id;
 
     const newReport = new Report({ userId, ...reportData });
     await newReport.save();
@@ -64,3 +62,36 @@ export const getAllReports = async (req, res) => {
     }
 };
 
+export const updateReport = async (req, res) => {
+  try {
+    const { reportId } = req.params;
+    const { reportData } = req.body;
+
+    const report = await Report.findById(reportId);
+    if (!report) {
+      return res.status(404).json({ message: 'Report not found' });
+    }
+
+    Object.assign(report, reportData);
+    await report.save();
+
+    return res.status(200).json({ message: 'Report updated successfully', report });
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+export const getUserReports = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const reports = await Report.find({ userId });
+    if (!reports) {
+      return res.status(404).json({ message: 'No reports found for this user' });
+    }
+
+    return res.status(200).json(reports);
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error', error });
+  }
+};
--- routes/ReportRoutes.js
+++ routes/ReportRoutes.js
@@ -1,15 +1,18 @@
 import express from "express";
-import {createReport, deleteReport, getReport, getAllReports} from "../controllers/ReportController.js";
+import {createReport, deleteReport, getReport, getAllReports, updateReport, getUserReports} from "../controllers/ReportController.js";
 import { authMiddleware } from "../middleware/middleware.js"; 
 
 
 
 const router = express.Router();
 
-router.post("/report", authMiddleware, createReport);
-router.delete("/:reportId", authMiddleware, deleteReport); 
-router.get("/:reportId", authMiddleware, getReport);
-router.get("/reports", getAllReports); 
+router.post("/report", authMiddleware, createReport);       // crete report
+router.delete("/:reportId", authMiddleware, deleteReport);  // delete report
+router.get("/reports", getAllReports);                      // get all reports
+router.patch("/:reportId", authMiddleware, updateReport);   // update report
+router.get("/mine", authMiddleware, getUserReports);        // get user reports
+router.get("/:reportId", authMiddleware, getReport);        // get report by id
+
 
 
 
