06f1d2197994654d932668d03cf50369c2c9e1f5
Giieck <g.petracin@gmail.com>
2025-06-28 18:54:11 +0200

    Atlas

--- utils/db.js
+++ utils/db.js
@@ -1,28 +1,3 @@
-/*import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const mongoConnect = async () => {
-    try {
-        const mongoURI = process.env.MONGO_URI;
-        if (!mongoURI) {
-            throw new Error('Mongo URI is not defined');
-        }
-
-        await mongoose.connect(mongoURI);
-
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-    }
-}
-
-export default mongoConnect;*/
-
-
-
-// utils/db.js
 import mongoose from 'mongoose';
 
 export const mongoConnect = async (uri = process.env.MONGO_URI) => {
