a72d282c566b90faf0f9564196e9fb6f8d4f6b87
Giieck <g.petracin@gmail.com>
2025-07-03 09:17:31 +0200

    API Orgs

--- controllers/OrgController.js
+++ controllers/OrgController.js
@@ -72,3 +72,70 @@ export const getAllUsers = async (req, res) => {
     return res.status(500).json({ message: 'Failed to fetch users', error });
   }
 };
+
+export const editOrg = async (req, res) => { 
+  try {
+    const { id } = req.params;  
+    const loggedInOrgId = req.org._id.toString();
+    const { email, username, password, phone, indirizzo } = req.body;
+
+    if (id !== loggedInOrgId) {
+      return res.status(403).json({ message: "You cannot edit another user's account." });
+    }
+
+    const org = await Org.findById(id);
+    if (!org) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (email) org.email = email;
+    if (username) org.username = username;
+    if (password) org.password = password;
+    if (phone) org.phone = phone;
+    if (indirizzo) org.indirizzo = indirizzo; 
+
+    await org.save();
+
+    return res.status(200).json({ message: 'User updated successfully', org });
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+//modificare e cancellare tutti i report indipendentemente dall'owner
+
+export const deleteReport = async (req, res) => {
+  try {
+    const { reportId } = req.params;
+
+    const report = await Report.findById(reportId);
+    if (!report) {
+      return res.status(404).json({ message: 'Report not found' });
+    }
+
+    await Report.findByIdAndDelete(reportId);
+
+    return res.status(200).json({ message: 'Report deleted successfully' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+export const updateReport = async (req, res) => {
+  try {
+    const { reportId } = req.params;
+    const { reportData } = req.body;
+
+    const report = await Report.findById(reportId);
+    if (!report) {
+      return res.status(404).json({ message: 'Report not found' });
+    }
+
+    Object.assign(report, reportData);
+    await report.save();
+
+    return res.status(200).json({ message: 'Report updated successfully', report });
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error', error });
+  }
+};
