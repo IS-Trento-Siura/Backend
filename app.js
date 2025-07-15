59fdd0a5bccd5a9ed8cc77c413209ca1e9962a74
HaoJie Zhan <zhanhaojie04@gmail.com>
2025-07-15 12:54:44 +0200

    prova

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     'http://localhost:8080', // per sviluppo locale
-    'https://frontend-t02s.onrender.com' // per deploy su render
+    'https://frontend-qn31.onrender.com' // per deploy su render
   ],
   credentials: true
 }));
